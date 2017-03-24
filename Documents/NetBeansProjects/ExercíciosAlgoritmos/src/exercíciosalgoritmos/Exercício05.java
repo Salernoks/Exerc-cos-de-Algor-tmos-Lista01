1f15eb62c52ab6c35f518cc3762a7918c8f1ea95
vitor.scsilva1 <vitor.scsilva1@CAS485743W10B21.senacsp.edu.br>
2017-03-24 18:08:26 -0300

    Exercício05

--- Documents/NetBeansProjects/ExercíciosAlgoritmos/src/exercíciosalgoritmos/Exercício05.java
+++ Documents/NetBeansProjects/ExercíciosAlgoritmos/src/exercíciosalgoritmos/Exercício05.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class Exercício05 {
     public static void main(String [] args){
         Scanner leitor = new Scanner (System.in);
-        double L1 = leitor.nextDouble(), a;
-        double L2 = 28;
+        double b, l = 2, a;
         System.out.println("àrea de um quadrado.");
         System.out.print("Digite a base do quadrado: ");
-        leitor.nextDouble();
-        a = (L1*L2);
-        System.out.println("A área do quadra é: "+a);
+        b = leitor.nextDouble();
+        a = b*l;
+        System.out.println("A área do quadrado é: "+a);
+        
+        
+        
+        
+        
+        
+        
+        
                 
         
         

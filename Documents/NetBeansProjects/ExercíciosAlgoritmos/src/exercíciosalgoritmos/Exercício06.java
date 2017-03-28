d3f6e4a7e896c6371476bd2f5e6497bae0b61040
Vitor Cordoba <vitor Cordoba>
2017-03-28 13:43:58 -0300

    Lista 01 Algoritmo Exercício 06

--- Documents/NetBeansProjects/ExercíciosAlgoritmos/src/exercíciosalgoritmos/Exercício06.java
+++ Documents/NetBeansProjects/ExercíciosAlgoritmos/src/exercíciosalgoritmos/Exercício06.java
@@ -3,23 +3,25 @@ package exercíciosalgoritmos;
     public class Exercício06 {
        public static void main (String [] args){
           Scanner leitor = new Scanner(System.in);
-          int A, B, C, a;
-          System.out.println("Escreva três números inteiros para calcular: ");
-          System.out.println("A) a área do triângulo retângulo que tem A por base e C por altura.");
-          System.out.print("Digite a base do triâmgulo: ");
-          A = leitor.nextInt();
-          System.out.print("Digite a altura do triângulo: ");
-          C = leitor.nextInt();
-          a = (A * C) / 2;
-          System.out.println(" A área do triângulo é "+a);
-          int r, Aa;
-          double pi, Aaa;
-          System.out.println("B) a área do círculo de raio C. (PI = 3.14159)." );
-            System.out.print("Digite o raio do circulo: ");
-            r = leitor.nextInt();
-            pi = 3.14159;
-            Aa = (r * r);
-            Aaa = pi * Aa;
-            System.out.print("A área do círculo é "+Aaa);
+        int A, B, C, a;
+           System.out.println("Escreva três números inteiros para calcular: ");
+           System.out.println("A) a área do triângulo retângulo que tem A por base e C por altura.");
+           System.out.print("Digite a base do triâmgulo: ");
+           A = leitor.nextInt();
+           System.out.print("Digite a altura do triângulo: ");
+           C = leitor.nextInt();
+           a = (A * C) / 2;
+           System.out.println(" A área do triângulo é "+a);
+        int r, Aa;
+        double pi, Aaa;
+           System.out.println("B) calcular a área do círculo de raio C. (PI = 3.14159)." );
+           System.out.print("Digite o raio do circulo: ");
+           r = leitor.nextInt();
+           pi = 3.14159;
+           Aa = (r * r);
+           Aaa = pi * Aa;
+           System.out.print("A área do círculo é "+Aaa);
+           System.out.println("c) Calcular a área do trapézio que tem A e B por bases e C por altura.");
+           System.out.println("Digite a base 1 do trapés");
     }
 }
